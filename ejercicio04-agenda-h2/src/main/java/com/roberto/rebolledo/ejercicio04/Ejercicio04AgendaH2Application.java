31cf38a7571d6b38b8b32ccb144e092294a278fa
Roberto Rebolledo <robertorebolledo.dev@gmail.com>
2025-09-30 23:38:16 +0200

    ejercicio4 relaciones H2

---
+++ ejercicio04-agenda-h2/src/main/java/com/roberto/rebolledo/ejercicio04/Ejercicio04AgendaH2Application.java
@@ -0,0 +1,13 @@
+package com.roberto.rebolledo.ejercicio04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio04AgendaH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio04AgendaH2Application.class, args);
+	}
+
+}
---
+++ ejercicio04-agenda-h2/src/test/java/com/roberto/rebolledo/ejercicio04/Ejercicio04AgendaH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberto.rebolledo.ejercicio04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio04AgendaH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
